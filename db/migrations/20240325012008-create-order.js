ba87f8883fa9b69d44e750857a110392a28adfbb
eligarc <elioenaig041294@gmail.com>
2024-03-25 17:29:47 -0600

    :construction: Se agrega importación de CUSTOMER_TABLE en la migración de create-order

--- db/migrations/20240325012008-create-order.js
+++ db/migrations/20240325012008-create-order.js
@@ -1,5 +1,6 @@
 'use strict';
 const { ORDER_TABLE } = require('./../models/order.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
 const { DataTypes, Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
